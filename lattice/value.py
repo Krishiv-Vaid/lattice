d29aedeff7e679cb70a4cf6d9dfa3bf9dd4c74dd
Krishiv Vaid <krish.vaid22@gmail.com>
2026-08-11 03:40:25 -0400

    feat(autograd): track scalar computation graph dependencies

--- lattice/value.py
+++ lattice/value.py
@@ -1,18 +1,31 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=""):
         self.data = float(data)
         self.grad = 0.0
+
+        self._prev = set(_children)
+        self._op = _op
+
+        self._backward = lambda: None
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
 
-        out = Value(self.data + other.data)
+        out = Value(
+            self.data + other.data,
+            (self, other),
+            "+"
+        )
 
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
 
-        out = Value(self.data * other.data)
+        out = Value(
+            self.data * other.data,
+            (self, other),
+            "*"
+        )
 
         return out
--- tests/test_value.py
+++ tests/test_value.py
@@ -3,10 +3,16 @@ from lattice.value import Value
 a = Value(2)
 b = Value(3)
 
-c = a + b
-d = a * b
+c = a * b
+d = c + a
 
 print("a =", a.data)
 print("b =", b.data)
-print("a + b =", c.data)
-print("a * b =", d.data)
+print("c =", c.data)
+print("d =", d.data)
+
+print("c operation:", c._op)
+print("d operation:", d._op)
+
+print("c has", len(c._prev), "parents")
+print("d has", len(d._prev), "parents")
