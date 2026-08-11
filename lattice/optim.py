8cb536b0fcbcfa95a8c58c247fe059bc5883786e
Krishiv Vaid <krish.vaid22@gmail.com>
2026-08-11 17:44:26 -0400

    feat(optim): extend SGD to update tensor parameters

--- lattice/optim.py
+++ lattice/optim.py
@@ -1,12 +1,65 @@
+from lattice.tensor import Tensor
+from lattice.value import Value
+
+
 class SGD:
-    def __init__(self, parameters, lr=0.01):
+    def __init__(
+        self,
+        parameters,
+        lr=0.01,
+    ):
         self.parameters = list(parameters)
         self.lr = lr
 
+        if self.lr <= 0:
+            raise ValueError(
+                "learning rate must be positive"
+            )
+
     def zero_grad(self):
         for parameter in self.parameters:
-            parameter.grad = 0.0
+            if isinstance(parameter, Tensor):
+                parameter.zero_grad()
+
+            elif isinstance(parameter, Value):
+                parameter.grad = 0.0
+
+            else:
+                raise TypeError(
+                    "SGD parameters must be "
+                    "Tensor or Value objects"
+                )
 
     def step(self):
         for parameter in self.parameters:
-            parameter.data -= self.lr * parameter.grad
+            if isinstance(parameter, Tensor):
+                if not parameter.requires_grad:
+                    continue
+
+                if parameter.grad is None:
+                    continue
+
+                for index in range(
+                    parameter.numel
+                ):
+                    parameter.data[index] -= (
+                        self.lr
+                        * parameter.grad[index]
+                    )
+
+            elif isinstance(parameter, Value):
+                parameter.data -= (
+                    self.lr
+                    * parameter.grad
+                )
+
+            else:
+                raise TypeError(
+                    "SGD parameters must be "
+                    "Tensor or Value objects"
+                )
+
+
+__all__ = [
+    "SGD",
+]
--- tests/test_optim.py
+++ tests/test_optim.py
@@ -1,32 +1,165 @@
 from lattice.optim import SGD
+from lattice.tensor import Tensor
 from lattice.value import Value
 
 
 def test_sgd_step():
-    parameter = Value(5.0)
-
-    parameter.grad = 2.0
+    parameter = Value(2.0)
+    parameter.grad = 3.0
 
     optimizer = SGD(
         [parameter],
-        lr=0.1
+        lr=0.1,
     )
 
     optimizer.step()
 
-    assert parameter.data == 4.8
+    assert parameter.data == 1.7
 
 
 def test_sgd_zero_grad():
-    parameter = Value(5.0)
-
-    parameter.grad = 7.0
+    parameter = Value(2.0)
+    parameter.grad = 3.0
 
     optimizer = SGD(
         [parameter],
-        lr=0.1
+        lr=0.1,
     )
 
     optimizer.zero_grad()
 
     assert parameter.grad == 0.0
+
+
+def test_tensor_sgd_step():
+    parameter = Tensor(
+        [
+            1.0,
+            2.0,
+            3.0,
+        ],
+        requires_grad=True,
+    )
+
+    parameter.grad = [
+        10.0,
+        20.0,
+        30.0,
+    ]
+
+    optimizer = SGD(
+        [parameter],
+        lr=0.1,
+    )
+
+    optimizer.step()
+
+    assert parameter.data == [
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+
+def test_tensor_sgd_zero_grad():
+    parameter = Tensor(
+        [
+            1.0,
+            2.0,
+            3.0,
+        ],
+        requires_grad=True,
+    )
+
+    parameter.grad = [
+        10.0,
+        20.0,
+        30.0,
+    ]
+
+    optimizer = SGD(
+        [parameter],
+        lr=0.1,
+    )
+
+    optimizer.zero_grad()
+
+    assert parameter.grad == [
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+
+def test_tensor_sgd_multiple_parameters():
+    weight = Tensor(
+        [
+            [1.0],
+            [2.0],
+        ],
+        requires_grad=True,
+    )
+
+    bias = Tensor(
+        [3.0],
+        requires_grad=True,
+    )
+
+    weight.grad = [
+        4.0,
+        5.0,
+    ]
+
+    bias.grad = [
+        6.0,
+    ]
+
+    optimizer = SGD(
+        [weight, bias],
+        lr=0.1,
+    )
+
+    optimizer.step()
+
+    assert weight.data == [
+        0.6,
+        1.5,
+    ]
+
+    assert bias.data == [
+        2.4,
+    ]
+
+
+def test_tensor_sgd_ignores_non_trainable_tensor():
+    parameter = Tensor([
+        1.0,
+        2.0,
+    ])
+
+    optimizer = SGD(
+        [parameter],
+        lr=0.1,
+    )
+
+    optimizer.step()
+
+    assert parameter.data == [
+        1.0,
+        2.0,
+    ]
+
+
+def test_sgd_rejects_nonpositive_learning_rate():
+    parameter = Value(1.0)
+
+    try:
+        SGD(
+            [parameter],
+            lr=0.0,
+        )
+
+        assert False
+
+    except ValueError:
+        pass
