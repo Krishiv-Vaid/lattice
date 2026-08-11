72d7a724c60b3a370d345e8308ec9baf28808f86
Krishiv Vaid <krish.vaid22@gmail.com>
2026-08-11 03:20:35 -0400

    Add a multiplication method within class Value in value.py

--- lattice/value.py
+++ lattice/value.py
@@ -9,3 +9,10 @@ class Value:
         out = Value(self.data + other.data)
 
         return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+ 
+        out = Value(self.data * other.data)
+
+        return out
