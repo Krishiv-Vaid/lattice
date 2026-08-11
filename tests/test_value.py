70810d7acf7241f04e8d0fa74a51f10954a22312
Krishiv Vaid <krish.vaid22@gmail.com>
2026-08-11 04:12:06 -0400

    feat(autograd): add reverse-mode backward graph traversal

--- tests/test_value.py
+++ tests/test_value.py
@@ -1,33 +1,13 @@
 from lattice.value import Value
 
-# Test addition backward
-a = Value(2.0)
-b = Value(3.0)
-
-c = a + b
-
-# Pretend a gradient of 1 has arrived at c
-c.grad = 1.0
-c._backward()
-
-print("Addition:")
-print("c =", c.data)
-print("a.grad =", a.grad)
-print("b.grad =", b.grad)
-
-print()
-
-# Test multiplication backward
-x = Value(2.0)
-y = Value(3.0)
+x = Value(3.0)
+y = Value(4.0)
 
 z = x * y
+loss = z * z
 
-# Pretend a gradient of 1 has arrived at z
-z.grad = 1.0
-z._backward()
+loss.backward()
 
-print("Multiplication:")
-print("z =", z.data)
-print("x.grad =", x.grad)
-print("y.grad =", y.grad)
+print("loss =", loss.data)
+print("dx =", x.grad)
+print("dy =", y.grad)
